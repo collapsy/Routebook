ff4f1ce366582268601eaefcc2842326a5163bd5
collapsy <38116052+collapsy@users.noreply.github.com>
2026-07-28 20:51:03 -0300

    feat: export traveler profile persistence

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,3 +1,4 @@
 export { closeDatabase, getDatabase } from "./client";
-export { trips } from "./schema";
+export { travelerProfiles, trips } from "./schema";
+export { DrizzleTravelerProfileRepository } from "./traveler-profile-repository";
 export { DrizzleTripRepository } from "./trip-repository";

2f9980361f5d2d316166ddcfdffea4ff41f981fa
collapsy <38116052+collapsy@users.noreply.github.com>
2026-07-29 07:30:07 -0300

    feat: export place repository

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,5 @@
 export { closeDatabase, getDatabase } from "./client";
-export { travelerProfiles, trips } from "./schema";
+export { places, travelerProfiles, trips } from "./schema";
+export { DrizzlePlaceRepository } from "./place-repository";
 export { DrizzleTravelerProfileRepository } from "./traveler-profile-repository";
 export { DrizzleTripRepository } from "./trip-repository";
